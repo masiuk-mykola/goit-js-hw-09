ffdd9c4a976160fb165037b2791e9463e03cbd7d
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-07-28 17:20:16 +0300

    part 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+// ==========refs======
+
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+
+const startBtnClickHandler = e => {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const stoptBtnClickHandler = e => {
+  clearInterval(timerId);
+  refs.body.style.backgroundColor = 'transparent';
+};
+
+// ==========EventListeners======
+refs.start.addEventListener('click', startBtnClickHandler);
+refs.stop.addEventListener('click', stoptBtnClickHandler);
