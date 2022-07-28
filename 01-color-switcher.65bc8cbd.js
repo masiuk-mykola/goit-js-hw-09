cd1dc63ea34ffc9f1b72953f303dbd26127631d0
masiuk-mykola <masiuk.mykola@gmail.com>
2022-07-28 14:36:46 +0000

    Deploying to gh-pages from @ masiuk-mykola/goit-js-hw-09@9df64b612f57c5313b308061179eee6501d841a4 🚀

--- 01-color-switcher.65bc8cbd.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};t.start.addEventListener("click",(e=>{timerId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(e=>{clearInterval(timerId),t.body.style.backgroundColor="transparent"}));
-//# sourceMappingURL=01-color-switcher.65bc8cbd.js.map
